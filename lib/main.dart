c9a01aa6ffc4d6ca19f96cded0ad6e25d155617f
kimemia21 <bobbymbogo71@gmail.com>
2024-12-24 09:33:11 +0300

    9:33

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,43 @@
-
+import 'package:application/firebase_options.dart';
 import 'package:application/widgets/Authentication/login.dart';
+import 'package:application/widgets/commsRepo/commsRepo.dart';
 import 'package:application/widgets/homepage.dart';
 import 'package:application/widgets/state/AppBloc.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        }
+
+        if (snapshot.hasData) {
+          return MotorbikePOSHomePage();
+        }
+
+        return const LoginScreen();
+      },
+    );
+
     return MultiProvider(
         providers: [
-          ChangeNotifierProvider(create: (context)=>Appbloc()),
+          ChangeNotifierProvider(create: (context) => Appbloc()),
         ],
         child: MaterialApp(
             debugShowCheckedModeBanner: false,
